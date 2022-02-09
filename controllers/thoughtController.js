1dffebf854da8d2096bc7958a56292579c1e90cb
Michael Hranek <mhranek@comcast.net>
2022-02-09 14:17:14 -0500

    Updated reactions controllers to allow posting reactions

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,25 +3,30 @@ const { User, Thought } = require('../models');
 module.exports = {
     getThoughts(req, res) {
         Thought.find()
+            .populate('reactions')
+            .select('-__v')
             .then((thoughts) => res.json(thoughts))
             .catch((err) => res.status(500).json(err));
     },
     postThought(req, res) {
         Thought.create(req.body)
+        .select('-__v')
             .then((thought) => {
                 User.findOneAndUpdate(
                     { _id: req.body.userId },
-                    { $addToSet: { thoughts: thought._id }},
+                    { $addToSet: { thoughts: thought._id } },
                     { runValidators: true, new: true }
                 )
-                .then((user) => !user ? res.status(404).json({ message: 'Thought created, but no user with that ID found' }) : res.status(201).json({ message: 'Thought added successfully'}))
+                    .then((user) => !user ? res.status(404).json({ message: 'Thought created, but no user with that ID found' }) : res.status(201).json({ message: 'Thought added successfully' }))
             })
             .catch((err) => res.status(500).json(err));
     },
     getThoughtById(req, res) {
         Thought.findOne({ _id: req.params.thoughtId })
-        .then((thought) => !thought ? res.status(404).json({ message: 'No thought with that ID' }) : res.json(thought))
-        .catch((err) => res.status(500).json(err));
+            .populate('reactions')
+            .select('-__v')
+            .then((thought) => !thought ? res.status(404).json({ message: 'No thought with that ID' }) : res.json(thought))
+            .catch((err) => res.status(500).json(err));
     },
     updateThought(req, res) {
         Thought.findOneAndUpdate(
@@ -29,19 +34,26 @@ module.exports = {
             { $set: req.body },
             { runValidators: true, new: true }
         )
+            .select('-__v')
+            .then((thought) => !thought ? res.status(404).json({ message: 'No thought with that ID' }) : res.json(thought))
+            .catch((err) => res.status(500).json(err));
+    },
+    deleteThought(req, res) {
+        Thought.findOneAndRemove({ _id: req.params.thoughtId })
+            .then((thought) => !thought ? res.status(404).json({ message: 'No thought with that ID' }) : res.json({ message: 'Successfully deleted thought' }))
+            .catch((err) => res.status(500).json(err));
+    },
+    addReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body }},
+            { runValidators: true, new: true }
+        )
         .select('-__v')
         .then((thought) => !thought ? res.status(404).json({ message: 'No thought with that ID' }) : res.json(thought))
         .catch((err) => res.status(500).json(err));
     },
-    deleteThought(req, res) {
-        Thought.findOneAndRemove({ _id: req.params.thoughtId })
-        .then((thought) => !thought ? res.status(404).json({ message: 'No thought with that ID' }) : res.json({ message: 'Successfully deleted thought' }))
-        .catch((err) => res.status(500).json(err));
-    },
-    addReaction(req, res) {
-        
-    },
     deleteReaction(req, res) {
-        
+
     }
 }
