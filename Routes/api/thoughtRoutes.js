c80ffaecbe9d816517e3017b35d8ad80a84f8fb4
Michael Hranek <mhranek@comcast.net>
2022-02-09 10:56:59 -0500

    Updated api routes

--- Routes/api/thoughtRoutes.js
+++ Routes/api/thoughtRoutes.js
@@ -1,3 +1,24 @@
 const router = require('express').Router();
+const {
+    getThoughts,
+    postThought,
+    getThoughtById,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
+
+// api/thoughts
+router.route('/').get(getThoughts).post(postThought);
+
+// api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+// api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction)
+
+// api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 module.exports = router;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,25 @@
 const User = require('../models/User');
 
 module.exports = {
-    
+    getUsers(req, res) {
+
+    },
+    createUser(req, res) {
+
+    },
+    getUserById(req, res) {
+
+    },
+    updateUser(req, res) {
+
+    },
+    deleteUser(req, res) {
+
+    },
+    addFriend(req, res) {
+
+    },
+    removeFriend(req, res) {
+        
+    }
 }
