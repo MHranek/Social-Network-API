551995eb5b2371735a268cf70dda6e85458d921a
Michael Hranek <mhranek@comcast.net>
2022-02-09 14:03:14 -0500

    Updated thoughts controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -8,17 +8,35 @@ module.exports = {
     },
     postThought(req, res) {
         Thought.create(req.body)
-            .then((thought) => res.json(thought))
+            .then((thought) => {
+                User.findOneAndUpdate(
+                    { _id: req.body.userId },
+                    { $addToSet: { thoughts: thought._id }},
+                    { runValidators: true, new: true }
+                )
+                .then((user) => !user ? res.status(404).json({ message: 'Thought created, but no user with that ID found' }) : res.status(201).json({ message: 'Thought added successfully'}))
+            })
             .catch((err) => res.status(500).json(err));
     },
     getThoughtById(req, res) {
-        
+        Thought.findOne({ _id: req.params.thoughtId })
+        .then((thought) => !thought ? res.status(404).json({ message: 'No thought with that ID' }) : res.json(thought))
+        .catch((err) => res.status(500).json(err));
     },
     updateThought(req, res) {
-        
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+        .select('-__v')
+        .then((thought) => !thought ? res.status(404).json({ message: 'No thought with that ID' }) : res.json(thought))
+        .catch((err) => res.status(500).json(err));
     },
     deleteThought(req, res) {
-        
+        Thought.findOneAndRemove({ _id: req.params.thoughtId })
+        .then((thought) => !thought ? res.status(404).json({ message: 'No thought with that ID' }) : res.json({ message: 'Successfully deleted thought' }))
+        .catch((err) => res.status(500).json(err));
     },
     addReaction(req, res) {
         
