69ebf2517954be75591b04c609faf3dd073521ce
Michael Hranek <mhranek@comcast.net>
2022-02-08 23:35:56 -0500

    Updated User and Thought Schemas

--- models/Thought.js
+++ models/Thought.js
@@ -1,7 +1,46 @@
 const mongoose = require('mongoose');
 
-const thoughtSchema = new mongoose.Schema({
+const reactionSchema = new mongoose.Schema({
+    reactionId: {
+        // TODO type of objectId, default value is a new objectId
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        // TODO 280 character maximum
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        // TODO Date, default value to current timestamp, use a getter method to format the timestamp on query
+    }
+})
 
+const thoughtSchema = new mongoose.Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        // TODO must be between 1 and 280 characters
+    },
+    createdAt: {
+        // TODO Date, default value to current timestamp, use a getter method to format the timestamp on query
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [
+        {
+            // TODO documents created with the reactionSchema
+        }
+    ]
+});
+
+thoughtSchema.virtual('reactionCount')
+.get(function() {
+    return this.reactions.length;
 });
 
 const Thought = mongoose.model('Thought', thoughtSchema);
--- models/User.js
+++ models/User.js
@@ -1,7 +1,33 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        // TODO Valid Email adress
+    },
+    thoughts: [
+        {
+            // TODO Thought model reference
+        }
+    ],
+    friends: [
+        {
+            // TODO User model reference
+        }
+    ]
+});
 
+userSchema.virtual('friendCount')
+.get(function() {
+    return this.friends.length;
 });
 
 const User = mongoose.model('User', userSchema);
